ce671bf9f0391b47475810b17b2ed897b063050a
Paul Mouzas <Paul@Pauls-iMac.local>
2015-09-22 09:50:51 -0400

    fix method

--- inventory/wishdb.py
+++ inventory/wishdb.py
@@ -1,9 +1,9 @@
 import os
 import MySQLdb
 
-host = os.get("MYSQLDB_HOST")
-user = os.get("MYSQLDB_USER")
-passwd = os.get("MYSQLDB_PASSWD")
+host = os.environ.get("MYSQLDB_HOST")
+user = os.environ.get("MYSQLDB_USER")
+passwd = os.environ.get("MYSQLDB_PASSWD")
 
 class WishDB(object):
     def __init__(self):
